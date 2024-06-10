6c7907ad81610a490a15343a323155fb730a6fe1
Suposedly Sly Fox <50972379+SupposedlySlyFox@users.noreply.github.com>
2024-06-10 11:29:44 -0300

    Update server.js
    Tiny little error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve the HTML file at the root path
-app.use(express.static(path.join(process.cwd(), 'AmazonScrapper-main')));
+app.use(express.static(path.join(process.cwd())));
 
 // Function to fetch and parse Amazon search results
 const fetchAmazonResults = async (keyword) => {
